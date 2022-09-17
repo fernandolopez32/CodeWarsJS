cc6cdd4ec19aa283629f869a90b948c66657e978
Fernando Lopez <flopez00032@gmail.com>
2022-09-17 18:31:08 -0500

    loop that returns strings n amont of times

--- JS/Code-Wars.js
+++ JS/Code-Wars.js
@@ -2,4 +2,14 @@
 // uses an input; your program should return, "Hello, <name> how are you doing today?".
 function greet(name){
     return `Hello, ${name} how are you doing today?`;
-};
+}
+
+//Given an integer n and two other values, build an array of size n filled with these two values alternating.
+
+function alternate(n, firstValue, secondValue){
+    let accumulator = "";
+    for (let i = 0; i >= n; i++){
+        return
+    }
+
+}
